0279a1a741a3ee5638122741753c092f50eb48b9
volson1 <olsonvm2007@yahoo.com>
2025-11-29 18:02:01 -0600

    Pre-caching

--- sw.js
+++ sw.js
@@ -1,9 +1,38 @@
+const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/script.js',
+    '/css/styles.css',
+    '/img/icons/icon-48x48.png',
+    'https://kit.fontawesome.com/e72ace8b45.js',
+    'https://fonts.googleapis.com/css?family=Pridi',
+    'https://fonts.googleapis.com/css?family=Poppins'
+];
+
 // listening for service worker
 self.addEventListener('install', evt => {
-    console.log('service worker has been installed');
-})
+    //console.log('service worker has been installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching assests');
+            cache.addAll(assets);
+        })
+    )
+});
 
 // listening for active service worker
 self.addEventListener('activate', evt => {
-    console.log('service worker has been activated');
-})
+    //console.log('service worker has been activated');
+});
+
+// fetch event
+self.addEventListener('fetch', evt => {
+    //console.log('fetch event', evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    )
+});
