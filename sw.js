1e7fa13d6ff0db2db2ea07be6fd02fe6f21ab7d3
volson1 <olsonvm2007@yahoo.com>
2025-11-29 18:49:43 -0600

    Dynamic Caching

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const staticCacheName = 'site-static-v1';
+const dynamicCache = 'site-dynamic-v1';
 const assets = [
     '/',
     '/index.html',
@@ -7,8 +8,9 @@ const assets = [
     '/css/styles.css',
     '/img/icons/icon-48x48.png',
     'https://kit.fontawesome.com/e72ace8b45.js',
-    'https://fonts.googleapis.com/css?family=Pridi',
-    'https://fonts.googleapis.com/css?family=Poppins'
+    'https://ka-f.fontawesome.com'
+    // 'https://fonts.googleapis.com/css?family=Pridi',
+    // 'https://fonts.googleapis.com/css?family=Poppins'
 ];
 
 // listening for service worker
@@ -40,7 +42,12 @@ self.addEventListener('fetch', evt => {
     //console.log('fetch event', evt);
     evt.respondWith(
         caches.match(evt.request).then(cacheRes => {
-            return cacheRes || fetch(evt.request);
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCache).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes;
+                })
+            });
         })
     )
 });
